b60226c98ca3c0bf70a90ff40bba6f35afc9df17
nerdlabs001 <39112973+nerdlabs001@users.noreply.github.com>
2019-06-13 15:43:30 -0400

    Closing old fork

--- src/version.h
+++ src/version.h
@@ -30,11 +30,11 @@ static const int DATABASE_VERSION = 70509;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 60200;
+static const int PROTOCOL_VERSION = 1000000;
 
 // earlier versions not supported as of Feb 2012, and are disconnected
 static const int MIN_PROTO_VERSION = 209;
-static const int MIN_PROTO_VERSION_FORK = 60099;
+static const int MIN_PROTO_VERSION_FORK = 60199;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
@@ -42,8 +42,8 @@ static const int CADDR_TIME_VERSION = 31402;
 
 // only request blocks from nodes outside this range of versions
 static const int NOBLKS_VERSION_START = 00001;
-static const int NOBLKS_VERSION_END = 60020;
-static const int NOBLKS_VERSION_END_FORK = 60099;
+static const int NOBLKS_VERSION_END = 60199;
+static const int NOBLKS_VERSION_END_FORK = 60199;
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
