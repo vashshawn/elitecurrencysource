b9a87f06bd644755f8ff33ca56e825a782a8e28b
flapmin <flapmin@gmail.com>
2023-09-22 11:18:41 -0500

    nightly

--- src/main.cpp
+++ src/main.cpp
@@ -3361,7 +3361,7 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv)
         // Send the rest of the chain
         if (pindex)
             pindex = pindex->pnext;
-        int nLimit = 500;
+        int nLimit = 50;
         printf("getblocks %d to %s limit %d\n", (pindex ? pindex->nHeight : -1), hashStop.ToString().substr(0,20).c_str(), nLimit);
         for (; pindex; pindex = pindex->pnext)
         {
